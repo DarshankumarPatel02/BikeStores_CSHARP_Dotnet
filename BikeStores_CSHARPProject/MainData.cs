2a9a10d8c5eb4ec77f2c2d1a4088dc962388b116
Darshan Patel <97763139+DarshankumarPatel02@users.noreply.github.com>
2023-07-05 09:51:43 -0400

    Add files via upload

---
+++ BikeStores_CSHARPProject/MainData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BikeStores_CSHARPProject
+{
+    public partial class MainData : Form
+    {
+        public MainData()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            //Close Form
+            this.Close();
+        }
+
+        private void Change_Click(object sender, EventArgs e)
+        {
+            //Add Modify Delete Form Pop-up
+            FormAMD operation = new FormAMD();
+            operation.ShowDialog();
+        }
+
+        private void MainData_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'bikeStoresDataSet.customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.bikeStoresDataSet.customers);
+        }
+
+        private void Reload_Click(object sender, EventArgs e)
+        {
+            //Refreshing datagridview using DataAdapter(select query)
+            SqlConnection connection = BikeStoresDB.GetConnection();
+            SqlDataAdapter sda=new SqlDataAdapter(@"SELECT customer_id, first_name,last_name,email,street, city, state, zip_code FROM sales.customers ",connection);
+            DataTable dt=new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Query_Click(object sender, EventArgs e)
+        {
+            //Query Form Pop-up
+            QueryBuilder query = new QueryBuilder();
+            query.ShowDialog();
+        }
+    }
+}
---
+++ BikeStores_CSHARPProject/Validator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BikeStores_CSHARPProject
+{
+    public static class Validator
+    {
+        //Error Message
+        private static string title = "Entry Error";
+
+        //getter and setter
+        public static string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        //check if there is text in textbox
+        public static bool IsPresent(TextBox textBox)
+        {
+            if (textBox.Text == "")
+            {
+                MessageBox.Show(textBox.Tag + " is a required field.", Title);
+                textBox.Focus();
+                return false;
+            }
+            return true;
+        }
+
+        //check if entered value in textbox is INT32
+        public static bool IsInt32(TextBox textBox)
+        {
+            int number = 0;
+            if (Int32.TryParse(textBox.Text, out number))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(textBox.Tag + " must be an integer.", Title);
+                textBox.Focus();
+                return false;
+            }
+        }
+        //check if entered value in textbox is email or not.
+        public static bool IsValidEmail(TextBox textBox)
+        {
+            if (textBox.Text.IndexOf("@") == -1 ||
+                 textBox.Text.IndexOf(".") == -1)
+            {
+                MessageBox.Show(textBox.Tag + " must be a valid email address.",
+                    Title);
+                textBox.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
